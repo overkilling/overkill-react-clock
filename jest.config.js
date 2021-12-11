ebc745159516222fb59d263255f323839adcc0c8
Paulo Schneider <paulo@vw-dilab.com>
2021-12-11 16:11:41 +0000

    Remove unnecessry identiy-object-mapper

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   roots: ['./src'],
   setupFilesAfterEnv: ['./test-utils/setup.ts'],
-  moduleNameMapper: {
-    '\\.(css|less)$': 'identity-obj-proxy'
-  },
   testEnvironment: 'jsdom'
 }
